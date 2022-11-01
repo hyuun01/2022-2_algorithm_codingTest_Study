bcf0a98f46ae74739b40d07993ee674e45154b4e
hyuun01 <hyuun1501@gmail.com>
2022-11-01 19:35:53 +0900

    스터디

--- ch06 정렬/06_3_23_기출문제_국영수.py
+++ ch06 정렬/06_3_23_기출문제_국영수.py
@@ -1,7 +1,8 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 data = []
 for _ in range(n):
-    name, k, e, m = input().split()
+    name, k, e, m = sys.stdin.readline().split()
     data.append((name, int(k), int(e), int(m)))
 
 data.sort(key=lambda x:(-x[1], x[2], -x[3], x[0]))
