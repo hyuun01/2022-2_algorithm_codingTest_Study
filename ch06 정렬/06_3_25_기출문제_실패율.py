4c8f5100893e1e883180e6703cbda4f11edd8835
hyuun01 <hyuun1501@gmail.com>
2022-11-08 13:32:36 +0900

    ch06 완료

--- ch06 정렬/06_3_25_기출문제_실패율.py
+++ ch06 정렬/06_3_25_기출문제_실패율.py
@@ -1,19 +1,32 @@
+
 def solution(N, stages):
     answer = []
+    rate = [[i, 0, 0] for i in range(N+2)]   # i번째 stage가 answer[i][2]명
     data = [(x, stages[x]) for x in range(len(stages))]
     
     data.sort(key=lambda x: x[1])
     stagenum = data[0][1]
-    ind = 0
-    answer.append(0)
+    rate[stagenum][1] = 1
     for i in range(1, len(stages)):
         if data[i][1] == stagenum:
-            answer[ind] += 1
-            continue
-        while data[i][1] > stagenum:
-            stagenum += 1
-        answer.append(0)
-        ind += 1
+            rate[stagenum][1] += 1
+        else:
+            while data[i][1] > stagenum:
+                stagenum += 1
+            rate[stagenum][1] += 1
     
+    pplsum = len(stages)
+    for i in range(1, N + 1):
+        rate[i][2] = rate[i][1] / pplsum
+        pplsum -= rate[i][1]
+        if pplsum == 0:
+            break
+    
+    rate = rate[1:N+1]
+    rate.sort(key=lambda x : (-x[2], x[0]))
+    
+    for x in rate:
+        answer.append(x[0])
     
     return answer
+
